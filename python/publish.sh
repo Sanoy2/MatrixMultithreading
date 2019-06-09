3e1d3564e2d80252c161544353880afc43fc14e5
Krzysztof Tomkow <krzysztof.tomkow@gmail.com>
2019-06-09 17:15:31 +0200

    Publish fix

--- python/publish.sh
+++ python/publish.sh
@@ -2,5 +2,6 @@
 
 cp main.py ~/Thesis/Testing/MatrixMultithreading/python/
 cp matrix.py ~/Thesis/Testing/MatrixMultithreading/python/
-
+cp matrixes_list_determinant.py ~/Thesis/Testing/MatrixMultithreading/python/
+cp pair.py ~/Thesis/Testing/MatrixMultithreading/python/
 wait 
